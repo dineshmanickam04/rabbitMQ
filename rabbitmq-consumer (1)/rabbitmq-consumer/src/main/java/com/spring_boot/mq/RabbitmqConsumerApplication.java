277c537278d02b4f39495f929bdbf56663b16c84
Dinesh M <eeedineh57@gmail.com>
2025-04-10 12:57:50 +0530

    Added rabbitmq consumer and producer projects

---
+++ rabbitmq-consumer (1)/rabbitmq-consumer/src/main/java/com/spring_boot/mq/RabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqConsumerApplication.class, args);
+	}
+
+}
